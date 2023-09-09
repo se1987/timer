6d38c48b7185f71f01cfb152ffd2c12d5aa53ad4
se1987 <hk130_sy322_ivi_dec24_may4@yahoo.co.jp>
2023-09-09 22:01:32 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict'
+// 今日の日付を表示
+let date = new Date();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+let day = date.getDate();
+
+const minute = document.getElementById("minute");
+const second = document.getElementById("second");
+const startBtn = document.getElementById("start");
+const resetBtn = document.getElementById("reset");
+
+let countNumber = 90;
+minute.textContent = `${Math.floor(countNumber / 60)}分`;
+second.textContent = `${Math.floor(countNumber % 60)}秒`;
+
+startBtn.addEventListener("click",() => {
+    function countDown(){
+        countNumber--;
+        if (countNumber >= 0){
+            let minuteCount = Math.floor(countNumber / 60);
+            let secondCount = Math.floor(countNumber % 60);
+            minute.textContent = `${minuteCount}分`;
+            second.textContent = `${secondCount}秒`;
+        } else {
+            countNumber = 0;
+            clearInterval(intervalId);
+        }
+    }
+    setInterval(countDown, 1000);
+});
+
+resetBtn.addEventListener("click",() => {
+    location.reload();
+});
+
+document.getElementById("today").innerHTML = `${year}年${month}月${day}日`;
